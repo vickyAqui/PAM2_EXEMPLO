c6cdcbb818f6ba5cd3ec8490763e4ba163366765
EversonBacelli <everson.willian29@gmail.com>
2026-02-27 11:33:58 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -1,20 +1,110 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// IMPORTAÇÃO
+import {useState} from 'react';
+import {Text,View,TextInput,TouchableOpacity,StyleSheet} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+export default function Aplicacao(){
+
+  const [n1,setN1] = useState()
+  const [n2,setN2] = useState()
+  const [resultado,setResultado] = useState()
+
+  // função anonima function(){console.lo('fdadf')}
+  const somar = ()=>{
+    const r = parseInt(n1) + parseInt(n2)
+    setResultado(r)
+  }
+    
+  return(
+    <View style={styles.tudo}>
+
+      <View>
+        <Text style={styles.titulo}> Somando dois Valores </Text>
+      </View>
+
+      <View style={{margin:15}}>
+        <Text> Primeiro número: </Text>          
+        <TextInput              
+          style={styles.caixaDeTexto}
+          placeholder='Digite aqui o primeiro número'
+          placeholderTextColor='#6f6f6f'
+          autoFocus={true}
+          keyboardType={'numeric'}
+
+          onChangeText = {text =>setN1(text)}
+        />            
+      </View>           
+
+      <View style={{margin:15}}>
+        <Text> Segundo número:  </Text>          
+        <TextInput
+          style={styles.caixaDeTexto}
+          placeholder='Digite aqui o segundo número'
+          placeholderTextColor='#6f6f6f'              
+          keyboardType={'numeric'}
+          // evento para mudar o estado da caixa de texto
+          onChangeText = {text =>setN2(text)}
+        />            
+      </View>           
+
+      <View style={{margin:15}}>           
+
+        <TouchableOpacity 
+          style={styles.botao} 
+          onPress={()=>somar()}
+          activeOpacity={0.5}
+        >
+          <Text style={styles.textoBotao}> Somar </Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={{margin:15}}>
+          <Text style={styles.resposta}> Total da soma: {resultado}  </Text>
+      </View>
+      
     </View>
-  );
+);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  tudo:{
+    backgroundColor:'#000',
+    height:'100%',
+    width:'100%'
   },
+
+  titulo:{
+    color:'#d30f59',
+    fontSize:25,
+    textAlign:'center',
+    marginTop:25,
+    marginBottom:10    
+  },
+
+  caixaDeTexto:{
+    borderWidth:1,
+    borderColor:'#2f2f2f',
+    color:'#f2f2f2',
+    borderRadius:40,
+    paddingLeft: 25,
+    height:60    
+  },
+
+  botao:{
+    backgroundColor: '#d30f33ff',    
+    borderRadius: 25, 
+    paddingVertical: 15,
+    alignItems: 'center'    
+  },
+
+  textoBotao:{
+    color:'#fff',
+    fontSize:18
+  },
+
+  resposta:{
+    color:'#d30f59',
+    fontSize:25,
+    textAlign:'center',
+    marginTop:30
+  }
 });
